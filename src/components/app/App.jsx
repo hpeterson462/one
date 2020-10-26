cfcea0850708963c93ac75e4a6faf10d58acc292
Heather Peterson <hpeterson462@gmail.com>
2020-10-26 15:58:44 -0700

    router built

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as BrowserRouter,
+  Switch,
+  Route
+} from 'react-router-dom';
+
+const List = () => {
+  return <h1>Character List</h1>
+}
+
+const Detail = () => {
+  return <h1>Character</h1>
+}
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/" component={List} />
+        <Route exact path="/Detail/:id" component={Detail} />
+      </Switch>
+    </Router>
+  );
 }
