f90cb70bc059da3ebd84ae5bac1dfd46474d5fd0
Heather Peterson <hpeterson462@gmail.com>
2021-10-14 13:16:36 -0700

    ternary for dynamic img render

--- src/components/characters/characterDetail/CharacterDetail.jsx
+++ src/components/characters/characterDetail/CharacterDetail.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useCharacterById } from '../../../hooks/characters';
 import PropTypes from 'prop-types';
+import landOfShadow from '../../../assets/landOfShadow.png';
+import clouds from '../../../assets/clouds.png';
+import eaglesEyrie from '../../../assets/eaglesEyrie.png';
+import arda from '../../../assets/arda.png';
+import ettenmoors from '../../../assets/ettenmoors.png';
+import mordor from '../../../assets/mordor.png';
 import shire from '../../../assets/shire.png';
 import ironHills from '../../../assets/ironHills.png';
 import lothlorien from '../../../assets/lothlorien.png';
+import forest from '../../../assets/forest.png';
 import greatEastRoad from '../../../assets/greatEastRoad.png';
 
 const CharacterDetail = () => {
@@ -15,30 +22,68 @@ const CharacterDetail = () => {
 
   return (
     <>
-      <section data-testid="character">
-
-        <img
-          style={{ width: '25%' }}
-          src={character.race === 'Hobbit' ?
-            shire
-            : (character.race === 'Dwarf' ?
-              ironHills
-              : (character.race === 'Elf' ?
-                lothlorien
-                : greatEastRoad
-              )
-            )}
-          alt={character.race === 'Hobbit' ?
-            'The Shire'
-            : (character.race === 'Dwarf' ?
-              'The Iron Hills'
-              : (character.race === 'Elf' ?
-                'Lothlorien'
-                : 'The Great East Road'
-              )
-            )}
-        />
-
+      <img
+        style={{ width: '25%' }}
+        src={
+          character.race === 'Men,Wraith' ?
+            landOfShadow
+            : character.race === 'Great Eagles' ?
+              eaglesEyrie
+              : character.race === 'Stone-trolls' ?
+                ettenmoors
+                : character.race === 'Ainur' ?
+                  arda
+                  : character.race === 'Maiar' ?
+                    clouds
+                    : character.race === 'Hobbit' ?
+                      shire
+                      : character.race === 'Human' ?
+                        greatEastRoad
+                        : character.race === 'Men' ?
+                          greatEastRoad
+                          : character.race === 'Dwarf' ?
+                            ironHills
+                            : character.race === 'Dwarves' ?
+                              ironHills
+                              : character.race === 'Elf' ?
+                                lothlorien
+                                : character.race === 'Elves' ?
+                                  lothlorien
+                                  : character.race === 'NaN' ?
+                                    forest
+                                    : mordor
+        }
+        alt={
+          character.race === 'Men,Wraith' ?
+            'The Land of Shadow'
+            : character.race === 'Great Eagles' ?
+              'Eagles Eyrie'
+              : character.race === 'Stone-trolls' ?
+                'Ettenmoors'
+                : character.race === 'Ainur' ?
+                  'Arda'
+                  : character.race === 'Maiar' ?
+                    'Light filled clouds'
+                    : character.race === 'Hobbit' ?
+                      'The Shire'
+                      : character.race === 'Human' ?
+                        'The Great East Road'
+                        : character.race === 'Men' ?
+                          'The Great East Road'
+                          : character.race === 'Dwarf' ?
+                            'Iron Hills'
+                            : character.race === 'Dwarves' ?
+                              'Iron Hills'
+                              : character.race === 'Elf' ?
+                                'Lothlorien'
+                                : character.race === 'Elves' ?
+                                  'Lothlorien'
+                                  : character.race === 'NaN' ?
+                                    'Forest'
+                                    : 'Mordor'
+        }
+      />
+      <section data-testid='character'>
         <h1>{character.name}</h1>
         <h3>{character.race}</h3>
         <h3>{character.gender}</h3>
--- src/components/characters/characterDetail/CharacterDetail.test.js
+++ src/components/characters/characterDetail/CharacterDetail.test.js
@@ -1,19 +1,57 @@
 import React from 'react';
-import { render, cleanup } from '@testing-library/react';
+import { render, cleanup, waitFor, screen, getByTestId, getByText } from '@testing-library/react';
 import DetailPage from '../../app/App';
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
+import { getApiById } from '../../../services/api';
+
+jest.mock('../../../services/api.js');
 
 describe('CharacterDetail component', () => {
   afterEach(() => cleanup());
   it('renders character details', () => {
-    const { asFragment } = render(<MemoryRouter>
-      <DetailPage
-        characters={[{
-          id: '5cd99d4bde30eff6ebccfbbe',
-          name: 'Adanel'
-        }]}
-      />
-    </MemoryRouter>);
+    const { asFragment } = render(
+      <MemoryRouter>
+        <DetailPage
+          characters={[{
+            id: '5cd99d4bde30eff6ebccfbbe',
+            name: 'Adanel'
+          }]}
+        />
+      </MemoryRouter>
+    );
     expect(asFragment()).toMatchSnapshot();
+  });
+
+  it('displays character detail after loading', () => {
+    getApiById.mockResolvedValue({
+      id: '5cd99d4bde30eff6ebccfbbe',
+      name: 'Adanel',
+      race: 'Human',
+      gender: 'Female',
+      wikiUrl: 'http://lotr.wikia.com//wiki/Adanel'
+    });
+    render(
+      <MemoryRouter initialEntries={['/details/5cd99d4bde30eff6ebccfbbe']}>
+        <Route
+          path='/details/:id'
+          component={DetailPage}
+        />
+      </MemoryRouter>
+    );
+
+    // screen.getByText('Loading...');
+
+    expect(screen.getByTestId('character')).toBe(
+      'Adanel'
+    );
+
+    return waitFor(() => {
+      screen.getByTestId('character').toBe(
+        'Adanel');
+      // screen.getByText('Adanel');
+      // screen.getByText('Human');
+      // screen.getByText('Female');
+      // screen.getByText('http://lotr.wikia.com//wiki/Adanel');
+    });
   });
 });
